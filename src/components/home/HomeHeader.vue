ef78cfca525d7f0924cc894c9d1544918671fc5b
carry <carrywu0313@126.com>
2018-10-29 16:25:38 +0800

    header

---
+++ src/components/home/HomeHeader.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <i class="icon iconfont icon-fanhui"></i>
+    </div>
+    <div class="header-input">
+      <i class="icon iconfont icon-sousuo"></i>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <i class="icon iconfont icon-jiantouxia"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style  lang="stylus" scoped>
+   @import "~styles/common.styl" //代码优化，在style引入文件要加 ~
+  .header
+    display: flex
+    height: .86rem
+    line-height: .86rem
+    background: $travelColor
+    color: #fff
+    .header-left
+      width: .64rem
+      float: left
+      .icon-fanhui
+        font-size .4rem
+        margin-left .2rem
+     .header-input
+       flex:1
+       height: .62rem
+       line-height: .62rem
+       margin-top .12rem
+       margin-left .2rem
+       padding-left .2rem
+       background: #ffffff
+       border-radius .1rem
+       color: #ccc
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align: center
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 引入重置样式
+import 'styles/reset.css'
+// 在移动端存在 1px像素问题
+import 'styles/border.css'
+// 在移动端的点击事件存在 300毫秒的延迟，影响用户体验
+// 借用fastclick库来解决: npm install fastcliak --save
+import fastClick from 'fastclick'
+// 引入字体图标
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
